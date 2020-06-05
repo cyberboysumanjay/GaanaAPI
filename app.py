d3c02eda0e2bf6202f2e78af742b35085b48cdd7
cyberboysumanjay <cyberboysumanjay@gmail.com>
2020-06-05 16:49:22 +0530

    Added Optional Lyrics Fetching

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
-from flask import Flask, render_template,request,flash,redirect
+from flask import Flask, render_template, request, flash, redirect
 import time
-from flask import  jsonify,json
+from flask import jsonify, json
 import gaana
 from traceback import print_exc
 app = Flask(__name__)
-app.secret_key = 'test'
+app.secret_key = 'thisisasecretkeyforgaanaapiokbye!'
+
 
 @app.route('/')
 def home():
-   return redirect("https://cyberboysumanjay.github.io/GaanaAPI/")
+    return redirect("https://cyberboysumanjay.github.io/GaanaAPI/")
+
 
 @app.route('/result/', methods=['GET', 'POST'])
 def result():
-    data=''
-    link=request.args.get('url')
+    data = ''
+    link = request.args.get('url')
+    lyrics = False
+    lyrics_ = request.args.get('lyrics')
+    if lyrics_ and lyrics_.lower() != 'false':
+        lyrics = True
     try:
-        data=(gaana.downloadAndParsePage(link))
-        if len(data)>0:
+        data = (gaana.downloadAndParsePage(link, lyrics))
+        if len(data) > 0:
             return jsonify(data)
         raise ArithmeticError
     except Exception as e:
-        errors=[]
-        error = {'title' : 'Unable to fetch',
-                'album' : 'Unable to fetch',
-                'thumb' : 'Unable to fetch',
-                'language' : 'Unable to fetch',
-                'gaana_url' : 'Unable to fetch',
-                'duration' : 'Unable to fetch',
-                'artist' : 'Unable to fetch',
-                'released' : 'Unable to fetch',
-                'bitrate' : 'Unable to fetch',
-                'link' : 'Unable to fetch',
-                'status': e
+        errors = []
+        error = {
+            'status': False,
+            'error': e
         }
         errors.append(error)
         return jsonify(errors)
     return data
 
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0',port=5000,use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, use_reloader=True)
--- gaana.py
+++ gaana.py
@@ -8,7 +8,10 @@ from traceback import print_exc
 import base64
 import json
 
-unpad = lambda s : s[0:-ord(s[-1])]
+
+def unpad(s): return s[0:-ord(s[-1])]
+
+
 REGEX = re.compile('> ({[^<]*}) <')
 JSONDEC = JSONDecoder()
 DOWN_FOLDER = '.'
@@ -16,66 +19,74 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'
 }
 
+
 def fate_proxy():
-    resp=requests.get('https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
-    a=((resp.text).split('\n'))
-    p_list=[]
+    resp = requests.get(
+        'https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
+    a = ((resp.text).split('\n'))
+    p_list = []
 
     for i in a:
         try:
             p_list.append(json.loads(i))
         except Exception as e:
-            #print("Test",e)
+            # print("Test",e)
             continue
-    #print(len(p_list))
-    np_list=[]
+    # print(len(p_list))
+    np_list = []
     for i in p_list:
-        if i['country']=='IN':
+        if i['country'] == 'IN':
             np_list.append(i)
-    proxy=[]
+    proxy = []
     for i in np_list:
         proxy.append((str(i['host'])+':'+str(i['port'])))
     return(proxy)
 
+
 def fix_share_url(s):
-    s=s.replace("\\",'')
-    s='https://gaana.com'+s
+    s = s.replace("\\", '')
+    s = 'https://gaana.com'+s
     return (s)
 
+
 def fix_album_art(url):
-    url=url.replace("175x175","640x640")
+    url = url.replace("175x175", "640x640")
     return url
+
 
 def decryptLink(message):
     IV = 'asd!@#!@#@!12312'.encode('utf-8')
     KEY = 'g@1n!(f1#r.0$)&%'.encode('utf-8')
     aes = AES.new(KEY, AES.MODE_CBC, IV)
-    #message=message.encode('utf-8')
+    # message=message.encode('utf-8')
     return unpad((aes.decrypt(base64.b64decode(message))).decode('utf-8'))
 
-def fix_artist_name(t):
-    t=t.split(',')
-    l=[]
-    for i in t:
-      i=i.split('#')[0]
-      l.append(i)
 
-    singers=''
+def fix_artist_name(t):
+    t = t.split(',')
+    l = []
+    for i in t:
+        i = i.split('#')[0]
+        l.append(i)
+
+    singers = ''
     for i in l:
-      singers=singers+i+', '
-    singers=singers[:len(singers)-2]
+        singers = singers+i+', '
+    singers = singers[:len(singers)-2]
     return singers
 
-def downloadAndParsePage(link):
-    response=''
-    response=requests.get(link,headers=headers).text
+
+def downloadAndParsePage(link, lyrics):
+    response = ''
+    response = requests.get(link, headers=headers).text
     raw_songs = list(set(REGEX.findall(response)))
-    if len(raw_songs)==0:
+    if len(raw_songs) == 0:
         try:
-            proxies=fate_proxy()
+            proxies = fate_proxy()
             for proxy in proxies:
                 try:
-                    response = requests.get(link,headers=headers,proxies={"http": proxy, "https": proxy}).text
+                    response = requests.get(link, headers=headers, proxies={
+                                            "http": proxy, "https": proxy}).text
                     break
                 except Exception as e:
                     print(e)
@@ -97,20 +108,33 @@ def downloadAndParsePage(link):
                 enc_message = json_song['path']['normal'][0]
             else:
                 enc_message = json_song['path']['auto'][0]
-
-
-            song = {'title' : json_song['title'],
-                    'album' : json_song['albumtitle'],
-                    'thumb' : fix_album_art(json_song['albumartwork_large']),
-                    'language' : json_song['language'],
-                    'gaana_url' : fix_share_url(json_song['share_url']),
-                    'duration' : str(int(json_song['duration'])//60)+"min " + str(int(json_song['duration'])%60) + "sec",
-                    'artist' : fix_artist_name(json_song['artist']),
-                    'released' : json_song['release_date'],
-                    'bitrate' : enc_message['bitRate'],
-                    'link' : decryptLink(enc_message['message'])
+            song = {
+                'status': True,
+                'title': json_song['title'],
+                'album': json_song['albumtitle'],
+                'thumb': fix_album_art(json_song['albumartwork_large']),
+                'language': json_song['language'],
+                'gaana_url': fix_share_url(json_song['share_url']),
+                'duration': str(int(json_song['duration'])//60)+"min " + str(int(json_song['duration']) % 60) + "sec",
+                'artist': fix_artist_name(json_song['artist']),
+                'released': json_song['release_date'],
+                'bitrate': enc_message['bitRate'],
+                'link': decryptLink(enc_message['message'])
             }
+            if lyrics:
+                song['lyrics'] = get_lyrics(song['gaana_url'])
             songs.append(song)
         except Exception as e:
             print(e)
     return songs
+
+
+def get_lyrics(link):
+    if '/song/' in link:
+        link = link.replace('/song/', '/lyrics/')
+    if '/lyrics/' in link:
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, 'lxml')
+        res = soup.find('div', class_='seelyrics')
+        return (res.get_text().strip())
+    return None
